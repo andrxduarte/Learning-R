1adc9e67384da3959c49cbfaf270b11678bb376a
André Duarte <101840230+andrxduarte@users.noreply.github.com>
2022-09-30 18:09:37 -0300

    Add files via upload

---
+++ calculadora.R
@@ -0,0 +1,26 @@
+> 2+2
+> 2*2
+> 2/2
+> 2-2
+> 2^2 #exponencial
+
+?log
+
+> sqrt(9) # Tira a raiz quadrada dos argumentos entre parênteses, no caso 9
+> sqrt(3*3^2) # raiz quadrada de 27
+> sqrt((3*3)^2)# raiz quadrada de 81
+
+> #prod é a função para multiplicação
+> prod(2,2) # O mesmo que 2x2
+> prod(2,2,3,4)# 2x2x3x4
+
+#log é a função para calcular o logaritmo
+> log(3) # log natural de 3
+> log(3,10)# log de 3 na base 10
+> log10(3)# o mesmo que acima! log 3 na base 10
+
+#abs é a função para pegar os valores em módulo
+> abs(3-9) # abs = modulo, |3-9|
+
+#Para fazer o fatorial de algum número use factorial()
+> factorial(4) #4 fatorial (4!)
